c48ba78402539846f47f1c02a63d3cd0eb978774
hailm <hai.lemanh.1310@gmail.com>
2022-03-25 18:56:27 +0700

    update

--- index.php
+++ index.php
@@ -1,5 +1,42 @@
 <?php $data["title"] = "Trang chủ"; ?>
 <?php $body = '' ?>
+<?php $bg = 'images/BG.png' ?>
 <?php require "template-parts/layouts/header.php"; ?>
-
+<div class="home__section1 uk-section">
+    <div class="uk-container uk-container-large uk-container-expand-right">
+        <div class="uk-flex-middle" uk-grid>
+            <div class="uk-width-auto@m">
+                <div class="home__section1__boxLeft">
+                    <div class="home__section1__boxLeft__item">
+                        <div class="home__section1__boxLeft__title">Lagom Studio</div>
+                        <div class="home__section1__boxLeft__txt1">Architecture & Interior</div>
+                    </div>
+                    <div class="home__section1__boxLeft__item">
+                        <div class="home__section1__boxLeft__txt2">Lagom là một trong những đơn vị uy tín hoạt động trong lĩnh vực tư vấn thiết kế, sản xuất và thi công nội ngoại thất. Luôn lấy sự hài lòng của khách hàng và chất lượng các công trình là mục tiêu hướng tới.</div>
+                    </div>
+                    <div class="home__section1__boxLeft__item">
+                        <a href="" class="home__section1__boxLeft__btnMore uk-button uk-button-default"><span>Xem thêm</span></a>
+                    </div>
+                </div>
+            </div>
+            <div class="uk-width-expand">
+                <div class="uk-border-pill uk-cover-container home__section1__radius">
+                    <canvas width="1920" height="1080"></canvas>
+                    <video class="uk-border-pill home__section1__radius uk-position-center block-video_video" autoplay="" loop="" muted="" playsinline="" poster="images/bg-video.png">
+                        <source src="videos/spark_vision_video_2018052113.webm" type="video/webm">
+                        <source src="videos/spark_vision_video_2018052113.mp4" type="video/mp4">
+                    </video>
+                    <div class="uk-border-pill home__section1__radius home__section1__overlay uk-overlay uk-overlay-primary uk-position-cover"></div>
+                    <a href="#modal-media-youtube" uk-toggle class="uk-position-center home__section1__btnPlay" data-src="images/btn-play.png" uk-img></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div id="modal-media-youtube" class="uk-flex-top" uk-modal>
+    <div class="uk-modal-dialog uk-width-auto uk-margin-auto-vertical">
+        <button class="uk-modal-close-outside" type="button" uk-close></button>
+        <iframe src="https://www.youtube-nocookie.com/embed/c2pz2mlSfXA" width="1920" height="1080" uk-video uk-responsive></iframe>
+    </div>
+</div>
 <?php require "template-parts/layouts/footer.php"; ?>
--- template-parts/layouts/header.php
+++ template-parts/layouts/header.php
@@ -2,23 +2,67 @@
 <html lang="vi">
 <head>
     <meta charset="UTF-8">
-    <title>BASE - <?= /** @var TYPE_NAME  */
+    <title>Lagom - <?= /** @var TYPE_NAME  */
         (isset($data['title'])) ? $data['title'] : ''; ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
     <!--CSS-->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <!-- UIkit CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.9.4/dist/css/uikit.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.13.1/dist/css/uikit.min.css" />
     <link rel="stylesheet" href="style.css?v=<?php echo(time()) ?>">
     <!--JS-->
     <script src="https://cdn.jsdelivr.net/npm/@shinsenter/defer.js/dist/defer_plus.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <!-- UIkit JS -->
-    <script src="https://cdn.jsdelivr.net/npm/uikit@3.9.4/dist/js/uikit.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/uikit@3.9.4/dist/js/uikit-icons.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.13.1/dist/js/uikit.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.13.1/dist/js/uikit-icons.min.js"></script>
     <script src="js/app.js?v=<?php echo(time()) ?>"></script>
 </head>
 <body class="<?= (isset($body))?$body:'' ?>">
 <?php require "template-parts/commons/facebook_chat.php"; ?>
 <!--app-->
-<div id="app" class="uk-height-viewport uk-offcanvas-content uk-overflow-hidden uk-position-relative">
+<div id="app" class="uk-height-viewport uk-background-norepeat uk-background-top-center uk-offcanvas-content uk-overflow-hidden uk-position-relative" data-src="<?= (isset($bg))?$bg:'' ?>" uk-img>
+<div class="header">
+    <div class="uk-container uk-container-large uk-padding-remove">
+        <nav class="uk-navbar-container uk-navbar-transparent" uk-navbar>
+            <div class="uk-navbar-left">
+                <a class="uk-navbar-toggle uk-hidden@m header__menu" href="#">
+                    <span uk-navbar-toggle-icon></span> <span class="uk-margin-small-left">Menu</span>
+                </a>
+                <a href="" class="uk-navbar-item uk-logo header__logo uk-visible@m"><img src="images/logo-thiet-ke.png" alt=""></a>
+            </div>
+            <div class="uk-navbar-center">
+                <a href="" class="uk-navbar-item uk-logo header__logo uk-hidden@m"><img src="images/logo-thiet-ke.png" alt=""></a>
+                <ul class="uk-navbar-nav uk-visible@m">
+                    <li class="uk-active"><a href="#">Trang chủ</a></li>
+                    <li><a href="#">Về chúng tôi</a></li>
+                    <li><a href="#">Dự án</a></li>
+                    <li><a href="#">Cửa hàng</a></li>
+                    <li><a href="#">Liên hệ</a></li>
+                </ul>
+            </div>
+            <div class="uk-navbar-right">
+                <div class="uk-navbar-item">
+                    <div uk-form-custom="target: > * > span:first-child">
+                        <select>
+                            <option value="1">VN</option>
+                            <option value="2">EN</option>
+                        </select>
+                        <span class="uk-link header__lang">
+                            <span></span>
+                            <span uk-icon="icon: chevron-down"></span>
+                        </span>
+                    </div>
+                </div>
+                <a href="" class="uk-navbar-item">
+                    <div class="uk-position-relative">
+                        <div class="uk-cover-container header__bag" data-src="images/bag.png" uk-img>
+                            <canvas width="29" height="35"></canvas>
+                        </div>
+                        <span class="header__bag__txt uk-badge uk-position-bottom-right">0</span>
+                    </div>
+                </a>
+            </div>
+        </nav>
+    </div>
+</div>
