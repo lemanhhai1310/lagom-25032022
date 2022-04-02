9672f4dc30f7d1c5da2102091ba6625295fafa86
hailm <hai.lemanh.1310@gmail.com>
2022-04-02 17:06:06 +0700

    update

--- template-parts/layouts/footer.php
+++ template-parts/layouts/footer.php
@@ -1,3 +1,32 @@
+<div class="footer">
+    <div class="uk-container uk-container-large uk-padding-remove">
+        <div class="footer__box">
+            <div class="uk-grid-collapse uk-grid-match" uk-grid>
+                <div class="uk-width-auto@m">
+                    <div class="uk-cover-container">
+                        <iframe class="uk-position-cover" src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d29805.518883674977!2d105.7882112!3d20.964966399999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1svi!2s!4v1648830564464!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        <canvas width="566" height="455"></canvas>
+                    </div>
+                </div>
+                <div class="uk-width-expand">
+                    <div class="footer__box1 uk-flex uk-flex-middle">
+                        <div class="uk-width-1-1">
+                            <div class="uk-flex-center" uk-grid>
+                                <div class="uk-width-3-5@m">
+                                    <div class="footer__box2">
+                                        <div class="footer__box1__map">A7-AnBinh city, KDT thành phố Giao Lưu,Phạm Văn Đồng, Cổ Nhuế, Hà Nội</div>
+                                        <div class="footer__box1__txt">Let’s start something <br class="uk-visible@m"> big together</div>
+                                        <div class="footer__box1__phone">094 689 42 36</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 </div>
 <!--/app-->
 </body>
